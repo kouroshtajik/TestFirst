ef281294d7578b95b736ad7a1486dfeb51f4ae46
kouroshtj74 <43277649+kouroshtj74@users.noreply.github.com>
2021-08-06 12:39:54 +0430

    Initial commit

---
+++ app/src/main/java/com/example/testfirst/TestConstraint.java
@@ -0,0 +1,14 @@
+package com.example.testfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestConstraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_constraint);
+    }
+}
