872b15163d9602dcf3ba151f64717f797ea31fd9
kouroshtj74 <43277649+kouroshtj74@users.noreply.github.com>
2021-08-26 20:17:40 +0430

    webView

---
+++ app/src/main/java/com/example/testfirst/TestWebView.java
@@ -0,0 +1,21 @@
+package com.example.testfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class TestWebView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_web_view);
+
+        WebView web = findViewById(R.id.web);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("https://pouyaheydari.com");
+    }
+}
