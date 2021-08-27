e251087ab7b5b02aed190fb61d89c5d53781a303
kouroshtj74 <43277649+kouroshtj74@users.noreply.github.com>
2021-08-27 12:48:10 +0430

    webView

---
+++ app/src/main/java/com/example/testfirst/TestDrawerLayout.java
@@ -0,0 +1,14 @@
+package com.example.testfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestDrawerLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_drawer_layout);
+    }
+}
